8aae99b4a26635a06135cf5de5eaf72457aace20
aseemroshan <aseemroshan86@gmail.com>
2025-01-25 17:05:05 +0530

    intial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const env = require('dotenv').config()
+const db = require('./config/db');
+db();
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(process.env.PORT, ()=>{
+    console.log(`Server Running`);
+    
+})
+
+module.exports = app;
